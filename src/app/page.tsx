3295056fae4efcb98053470a9f97f3aa671bc849
wdpedroborges <xpedrostewart@gmail.com>
2024-03-25 16:08:02 -0300

    padronização visual dos formulários

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,12 +45,12 @@ export default function Home() {
       <Link href="/registro" className="block my-8 text-center hover:underline text-indigo-600">Ainda não tenho uma conta</Link>
       <h2 className="font-bold text-2xl text-center">Login</h2>
       <div className="h-1 bg-indigo-600 w-[100px] mx-auto my-4"></div>
-      <form onSubmit={(e) => handleSubmit(e)} className="mx-auto border p-4 rounded-xl w-full lg:w-1/2 flex flex-col">
-        <label htmlFor="cep" className="font-bold my-2">Nome de usuário: <span className="text-xs text-red-500">*</span></label>
-        <input required={true} type="text" id="cep" className="p-2 border rounded-md" onChange={(e) => setFormData({ ...formData, username: e.target.value })} />
-        <label htmlFor="state" className="font-bold my-2">Senha: <span className="text-xs text-red-500">*</span></label>
-        <input required={true} type="password" id="state" className="p-2 border rounded-md" onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
-        <button className="p-4 border rounded-xl text-center cursor-pointer w-1/3 mx-auto my-4 bg-gradient-to-tr from-indigo-600 to-indigo-800 text-white font-bold hover:opacity-95">Login</button>
+      <form onSubmit={(e) => handleSubmit(e)} className="mx-auto p-4 rounded-xl w-full lg:w-1/2 flex flex-col">
+        <label htmlFor="cep" className="font-bold my-4">Nome de usuário: <span className="text-xs text-red-500">*</span></label>
+        <input required={true} type="text" id="cep" className="py-4 px-2 rounded-xl border border-gray-400" onChange={(e) => setFormData({ ...formData, username: e.target.value })} />
+        <label htmlFor="state" className="font-bold my-4">Senha: <span className="text-xs text-red-500">*</span></label>
+        <input required={true} type="password" id="state" className="py-4 px-2 rounded-xl border border-gray-400" onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
+        <button className="bg-gray-100 text-black p-2 rounded-xl border border-gray-400 my-8 hover:bg-indigo-600 hover:text-white transition-all w-1/3 self-end">Login</button>
       </form>
     </>
   );
